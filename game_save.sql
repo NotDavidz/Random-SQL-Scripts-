2f67bae8752953de8c25168a76eb9517d3c749c2
NotDavidz <151092888+NotDavidz@users.noreply.github.com>
2025-03-20 18:46:32 -0400

    Add files via upload

--- game_save.sql
+++ game_save.sql
@@ -2,8 +2,7 @@ CREATE DATABASE GameDB;
 GO
 USE GameDB;
 GO
-
--- Player
+-- Player Table
 CREATE TABLE Players (
     id INT IDENTITY(1,1) PRIMARY KEY,
     name NVARCHAR(50),
@@ -11,61 +10,50 @@ CREATE TABLE Players (
     experience INT DEFAULT 0
 );
 GO
-
--- Inventory
+-- Inv Table 
 CREATE TABLE Inventory (
     id INT IDENTITY(1,1) PRIMARY KEY,
     player_id INT,
     item_name NVARCHAR(50),
     quantity INT DEFAULT 1,
-    -- Delete from other table if deleted
     FOREIGN KEY (player_id) REFERENCES Players(id) ON DELETE CASCADE
 );
 GO
-
--- Adding players
+-- Add
 INSERT INTO Players (name, level, experience) VALUES ('NotDavid', 5, 1200);
 INSERT INTO Players (name, level, experience) VALUES ('tacocaT', 3, 800);
 GO
 
--- Adding stuff to inventory
--- NotDavid
+-- Adding amongus to inventory
 INSERT INTO Inventory (player_id, item_name) VALUES (1, 'Sword');
 INSERT INTO Inventory (player_id, item_name, quantity) VALUES (1, 'Potion', 3);
--- Tacocat
 INSERT INTO Inventory (player_id, item_name) VALUES (2, 'Shield');
 INSERT INTO Inventory (player_id, item_name) VALUES (2, 'Axe');
 GO
 
---Joining 
 SELECT * FROM Inventory
 INNER JOIN Players ON Inventory.player_id = Players.id
-
--- Retrieve all players
+-- Ret players
 SELECT * FROM Players;
 GO
 
--- Ret. inventory for somebody 
+-- Ret. inventory for sm 
 SELECT i.item_name, i.quantity 
 FROM Inventory i
 JOIN Players p ON i.player_id = p.id
 WHERE p.name = 'NotDavid';
 GO
-
 -- Updating smth 
 UPDATE Players 
 SET level = 6, experience = 1500 
 WHERE name = 'NotDavid';
 GO
-
--- Delete player from table 
+-- Delete player from 
 DELETE FROM Players WHERE name = 'tacocaT';
 GO
-
--- Retrieve all players
+-- Print everyone
 SELECT * FROM Players;
 GO
-
 DROP TABLE Players;
 DROP TABLE Inventory;
 GO
